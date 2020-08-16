e5971a441c13c0476afa9dd6fbb37393c2977ab3
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-08-16 01:58:28 -0700

    Portfolio overhaul
    Additions
    -JS folder inside,
    -Jons_Portfolio.js
    
    in folder images
    -C_Sharp_and_Dot_NET.png
    -HTML_CSS_and_JavaScript.jpeg
    -Jon.jpg
    -SQL_and_Database.jpg
    
    Modifications to
    -jons_portfolio.css

---
+++ JS/Jons_Portfolio.js
@@ -0,0 +1,41 @@
+// SLIDE SHOW
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// NEXT AND PREVIOUS CONTROLS
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// IMAGE CONTROLS
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+// /SLIDE SHOW
+
+
+// OPEN AND CLOSE FORM
+function openForm(){
+    document.getElementById("myForm").style.display = "block";
+} 
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+// /OPEN AND CLOSE FORM
